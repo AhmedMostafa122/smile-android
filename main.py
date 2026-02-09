2d3eda3b38881d5e14a9a1302009d9e188445476
AhmedMostafa122 <111127703+AhmedMostafa122@users.noreply.github.com>
2026-02-09 19:48:48 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Multimedia Download Manager
+A modern application for downloading videos, audio, images, and documents.
+"""
+
+from ui import launch_ui
+
+if __name__ == "__main__":
+    launch_ui()
